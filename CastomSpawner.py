5117f57ca917436c888109e79f4c66b4cc71516e
vokin23 <kinbiceps@mail.ru>
2023-12-18 12:22:51 +0300

    Версия 4.0!

--- CastomSpawner.py
+++ CastomSpawner.py
@@ -3,13 +3,24 @@ import asyncio
 import json
 import re
 
-
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 client = discord.Client(intents=intents)
 
 message_data = {}
+
+
+def get_count_users(leader):
+    with open('CustomSpawnPlayerConfig.txt', 'r') as file:
+        data = file.read()  # Читаем содержимое файла
+
+    with open('config.json', 'r', encoding='utf-8') as file:
+        data1 = json.load(file)
+
+    substring = f'|{data1[leader]["group_id"]}|'
+    count = data.count(substring)
+    return count
 
 
 async def read_constant_from_json(constant: object) -> object:
@@ -22,11 +33,11 @@ async def read_constant_from_json(constant: object) -> object:
 
 
 def clear_zero_lines():
-    with open('CustomSpawnPlayerConfig.txt', 'r') as file:
+    with open('C:\\MGStalker\\servers\\StalkerRP\\profiles\\CustomSpawnPlayerConfig.txt', 'r') as file:
         lines = file.readlines()
         non_empty_lines = [line for line in lines if line.strip() != '']
 
-    with open('CustomSpawnPlayerConfig.txt', 'w') as file:
+    with open('C:\\MGStalker\\servers\\StalkerRP\\profiles\\CustomSpawnPlayerConfig.txt', 'w') as file:
         file.writelines(non_empty_lines)
 
 
@@ -39,10 +50,11 @@ def extract_numbers_with_seven(data):
 async def on_ready():
     print(f'{client.user} запустился!')
 
+
 @client.event
 async def on_message(message):
     if isinstance(message.channel, discord.TextChannel):
-        if message.channel.name == 'пропись':
+        if message.channel.name == 'общее':
             # Игнорируем сообщения ботов
             if message.author == client.user:
                 return
@@ -52,6 +64,9 @@ async def on_message(message):
 
             with open('config.json', 'r', encoding='utf-8') as file:
                 data = json.load(file)
+
+            with open('donat_slot.json', 'r', encoding='utf-8') as file:
+                donat_slots = json.load(file)
 
             if message.author.name in data:
                 if "прописать" in message.content.lower():
@@ -71,12 +86,27 @@ async def on_message(message):
                                             gp_name = value["leader"]
                                             await message.channel.send(f'{steam_id} уже находится группировке {gp_name}!')
 
-                        with open('CustomSpawnPlayerConfig.txt', 'a', encoding='utf-8') as file:
-                            if len(propisat) > 0:
-                                for item in propisat:
-                                    file.write(f'\n{item}|{data[message.author.name]["parametr"]}|'
-                                               f'{data[message.author.name]["group_id"]}|'
-                                               f'{data[message.author.name]["cords_spawner"]}')
+                        if get_count_users(message.author.name) < data[message.author.name]["slots"] and lines_inv.count(f'{steam_id}') == 0:
+                            with open('CustomSpawnPlayerConfig.txt', 'a', encoding='utf-8') as file:
+                                if len(propisat) > 0:
+                                    for item in propisat:
+                                        file.write(f'\n{item}|{data[message.author.name]["parametr"]}|'
+                                                   f'{data[message.author.name]["group_id"]}|'
+                                                   f'{data[message.author.name]["cords_spawner"]}')
+
+                        elif 10 <= get_count_users(message.author.name) < 15 and lines_inv.count(f'{steam_id}') == 0:
+                            try:
+                                if donat_slots[steam_id]["rank"] == "10-15":
+                                    with open('CustomSpawnPlayerConfig.txt', 'a', encoding='utf-8') as file:
+                                        if len(propisat) > 0:
+                                            for item in propisat:
+                                                file.write(f'\n{item}|{data[message.author.name]["parametr"]}|'
+                                                           f'{data[message.author.name]["group_id"]}|'
+                                                           f'{data[message.author.name]["cords_spawner"]}')
+
+                            except:
+                                await message.channel.send(f'Пропись невозможна!\n'
+                                                           f'Либо в групперовке уже занято {data[message.author.name]["slots"]}, либо у {steam_id} нет донат-слота!')
 
                     # Добавляем реакцию на сообщение
                     await message.add_reaction("👍")
@@ -99,7 +129,7 @@ async def on_message(message):
                         if should_add:
                             filtered_lines.append(line)
 
-                    with open('CustomSpawnPlayerConfig.txt', 'w') as file:
+                    with open('CustomSpawnPlayerConfig.txt', 'w', encoding='utf-8') as file:
                         file.writelines(filtered_lines)
 
                     clear_zero_lines()
